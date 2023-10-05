b0e79ae4dac5224181dc8ee10e4f95747de198df
Mehmet Doğru <mdogru685@gmail.com>
2023-10-05 16:07:04 +0300

    feat: added new

--- customer/src/app.js
+++ customer/src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const config = require("./config/index");
+const errorHandler = require("./middlewares/error-handler.middleware");
+const loaders = require("./loaders");
 
 config();
+loaders();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 app.get("/", (req, res) => {
   return res.status(200).json({ msg: "Hello from Customer Service" });
 });
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Customer Service running port on ${process.env.PORT}`);
