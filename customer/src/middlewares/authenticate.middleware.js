71a7ca5da3d4ddd6f9850183e08f0a6852a37eba
Mehmet Doğru <mdogru685@gmail.com>
2023-10-05 16:07:39 +0300

    feat: added auth middleware

---
+++ customer/src/middlewares/authenticate.middleware.js
@@ -0,0 +1,27 @@
+const httpStatus = require("http-status");
+const jwt = require("jsonwebtoken");
+const ApiError = require("../responses/error.response");
+
+async function verifyToken(req, res, next) {
+  try {
+    const authorization = req.header("Authorization");
+    if (!authorization) {
+      return next(new ApiError("Acces denied. No token provided", httpStatus.UNAUTHORIZED));
+    }
+
+    const token = authorization.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (error, decoded) => {
+      if (error) {
+        return next(new ApiError("Acces denied. Jwt Malformed", httpStatus.UNAUTHORIZED));
+      }
+
+      req.userId = decoded._id;
+      next();
+    });
+  } catch (error) {
+    return next(new ApiError(error.message, httpStatus.BAD_REQUEST));
+  }
+}
+
+module.exports = verifyToken;
