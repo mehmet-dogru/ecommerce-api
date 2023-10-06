6f643f71b60bba1f159a8d7bfde51002ccac3012
Mehmet Doğru <mdogru685@gmail.com>
2023-10-06 10:53:15 +0300

    feat: added register and login

--- customer/src/controllers/customer.controller.js
+++ customer/src/controllers/customer.controller.js
@@ -1,17 +1,39 @@
 const httpStatus = require("http-status");
+const { passwordToHash, generateAccessToken } = require("../scripts/utils/helper");
 const customerService = require("../services/customer.service");
 const ApiError = require("../responses/error.response");
 const successResponse = require("../responses/success.response");
 
 class CustomerController {
-  async create(req, res, next) {
+  async register(req, res, next) {
     try {
+      const hash = passwordToHash(req.body.password);
+
+      const customer = await customerService.create({ ...req.body, address: [], password: hash });
+
+      successResponse(res, httpStatus.CREATED, customer);
     } catch (err) {
       return next(new ApiError(err.message, httpStatus.BAD_REQUEST));
     }
   }
 
-  async list(req, res, next) {
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const hashedPassword = passwordToHash(password);
+
+      const customer = await customerService.findOne({ email, password: hashedPassword });
+
+      if (!customer) return next(new ApiError("Invalid email or password", httpStatus.BAD_REQUEST));
+
+      const token = generateAccessToken(customer);
+      successResponse(res, httpStatus.OK, token);
+    } catch (err) {
+      return next(new ApiError(err.message, httpStatus.BAD_REQUEST));
+    }
+  }
+
+  async profile(req, res, next) {
     try {
     } catch (err) {
       return next(new ApiError(err.message, httpStatus.BAD_REQUEST));
--- customer/src/routes/customer.route.js
+++ customer/src/routes/customer.route.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const customerController = require("../controllers/customer.controller");
 const validate = require("../middlewares/validate.middleware");
 const authenticate = require("../middlewares/authenticate.middleware");
+const validationCustomerSchema = require("../validations/customer.validation");
 
-router.post("/", authenticate);
-router.get("/", authenticate);
+router.post("/register", validate(validationCustomerSchema.registerSchema), customerController.register);
+router.post("/login", validate(validationCustomerSchema.loginSchema), customerController.login);
+
+router.get("/profile", authenticate, customerController.profile);
 
 module.exports = router;
