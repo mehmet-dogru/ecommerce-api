25a3cacfeb41c60bfb2e17497681153a73203da5
Mehmet Doğru <mdogru685@gmail.com>
2023-10-13 10:49:21 +0300

    feat: set app.js

--- shopping/src/app.js
+++ shopping/src/app.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const errorHandler = require("./middlewares/error-handler.middleware");
 const config = require("./config/index");
+const loaders = require("./loaders");
+const { orderRoutes, cartRoutes } = require("./routes");
 
 config();
+loaders();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
-app.get("/", (req, res) => {
+app.get("/whoami", (req, res) => {
   return res.status(200).json({ msg: "Hello from Shopping Service" });
 });
+
+app.use("/cart", cartRoutes);
+app.use("/order", orderRoutes);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Shopping Service running port on ${process.env.PORT}`);
